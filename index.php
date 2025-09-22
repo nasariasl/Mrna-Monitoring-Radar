1cf58bbb447e74836a4855958369086c17c06cf8
Mohammad Reza Nassari Asl <nasariasl@gmail.com>
2025-09-22 19:05:46 +0330

    Update index.php

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
     <!-- Navigation-->
 	<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #051103;">
 		<div class="w-100 d-flex justify-content-center px-4">
-			<a class="navbar-brand ms-auto" dir="rtl" href="">پنجره خدمات مانیتورینگ تحت وب</a>
+			<a class="navbar-brand" dir="rtl" href="">پنجره خدمات مانیتورینگ تحت وب</a>
 		</div>
 	</nav>
 	
